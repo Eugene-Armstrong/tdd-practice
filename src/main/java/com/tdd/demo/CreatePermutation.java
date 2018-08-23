9829e9fe905d36bd12de38b8092e1bc6e8684601
Eugene-Armstrong <1020454354@qq.com>
2018-08-23 14:14:49 +0800

    [Jeffery] finish should_return_abc_acb_bac_bca_cab_cba_when_input_abc (passed)

--- src/main/java/com/tdd/demo/CreatePermutation.java
+++ src/main/java/com/tdd/demo/CreatePermutation.java
@@ -1,13 +1,24 @@
 package com.tdd.demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CreatePermutation {
     public static List<String> cal(String input) {
-        if(input.length() == 2){
-            String tmp = input.substring(1) + input.substring(0,1);
-            return Arrays.asList(input,tmp);
+        if (input.length() == 2) {
+            String tmp = input.substring(1) + input.substring(0, 1);
+            return Arrays.asList(input, tmp);
+        }
+        if (input.length() == 3) {
+            List<String> permutations = new ArrayList<>();
+            permutations.add(input);
+            permutations.add(input.substring(0, 1) + input.substring(2) + input.substring(1, 2));
+            permutations.add(input.substring(1, 2) + input.substring(0, 1) + input.substring(2));
+            permutations.add(input.substring(1, 2) + input.substring(2) + input.substring(0, 1));
+            permutations.add(input.substring(2) + input.substring(0, 1) + input.substring(1, 2));
+            permutations.add(input.substring(2) + input.substring(1, 2) + input.substring(0, 1));
+            return permutations;
         }
         return Arrays.asList(input);
     }
