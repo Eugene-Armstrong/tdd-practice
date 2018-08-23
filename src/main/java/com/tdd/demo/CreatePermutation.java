8886899fc1e55bcd04525af269a7161c58db5824
Eugene-Armstrong <1020454354@qq.com>
2018-08-23 14:28:46 +0800

    [Jeffery+Tracy] finish should_return_24_when_input_abcd (passed)

--- src/main/java/com/tdd/demo/CreatePermutation.java
+++ src/main/java/com/tdd/demo/CreatePermutation.java
@@ -6,20 +6,34 @@ import java.util.List;
 
 public class CreatePermutation {
     public static List<String> cal(String input) {
-        if (input.length() == 2) {
-            String tmp = input.substring(1) + input.substring(0, 1);
-            return Arrays.asList(input, tmp);
+//        if (input.length() == 2) {
+//            String tmp = input.substring(1) + input.substring(0, 1);
+//            return Arrays.asList(input, tmp);
+//        }
+//        if (input.length() == 3) {
+//            List<String> permutations = new ArrayList<>();
+//            permutations.add(input);
+//            permutations.add(input.substring(0, 1) + input.substring(2) + input.substring(1, 2));
+//            permutations.add(input.substring(1, 2) + input.substring(0, 1) + input.substring(2));
+//            permutations.add(input.substring(1, 2) + input.substring(2) + input.substring(0, 1));
+//            permutations.add(input.substring(2) + input.substring(0, 1) + input.substring(1, 2));
+//            permutations.add(input.substring(2) + input.substring(1, 2) + input.substring(0, 1));
+//            return permutations;
+//        }
+        if(input.length() == 1){
+            return Arrays.asList(input);
         }
-        if (input.length() == 3) {
-            List<String> permutations = new ArrayList<>();
-            permutations.add(input);
-            permutations.add(input.substring(0, 1) + input.substring(2) + input.substring(1, 2));
-            permutations.add(input.substring(1, 2) + input.substring(0, 1) + input.substring(2));
-            permutations.add(input.substring(1, 2) + input.substring(2) + input.substring(0, 1));
-            permutations.add(input.substring(2) + input.substring(0, 1) + input.substring(1, 2));
-            permutations.add(input.substring(2) + input.substring(1, 2) + input.substring(0, 1));
-            return permutations;
+        List<String> permutations = new ArrayList<>();
+        for(int i = 0; i < input.length(); i++){
+            List<String> subPermutations = cal(removeOneCharByIndex(input, i));
+            for(String subPermutation:subPermutations){
+                permutations.add(input.charAt(i)+subPermutation);
+            }
         }
-        return Arrays.asList(input);
+        return permutations;
+    }
+
+    private static String removeOneCharByIndex(String input, int i){
+        return input.substring(0,i)+input.substring(i+1);
     }
 }
