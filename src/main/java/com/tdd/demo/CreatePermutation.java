f2a2213d6e2b012754f5be23ebf9ca15e3def8ea
Eugene-Armstrong <1020454354@qq.com>
2018-08-23 14:03:14 +0800

    [Jeffery] finish should_return_ab_ba_when_input_ab (passed)

--- src/main/java/com/tdd/demo/CreatePermutation.java
+++ src/main/java/com/tdd/demo/CreatePermutation.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class CreatePermutation {
     public static List<String> cal(String input) {
+        if(input.length() == 2){
+            String tmp = input.substring(1) + input.substring(0,1);
+            return Arrays.asList(input,tmp);
+        }
         return Arrays.asList(input);
     }
 }
